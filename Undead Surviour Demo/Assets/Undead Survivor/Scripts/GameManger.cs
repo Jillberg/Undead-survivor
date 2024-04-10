29669dc86c212330987a42abcce9327452c26f63
Jillberg <105028461+Jillberg@users.noreply.github.com>
2024-04-11 00:49:48 +0800

    Update GameManger.cs

--- Undead Surviour Demo/Assets/Undead Survivor/Scripts/GameManger.cs
+++ Undead Surviour Demo/Assets/Undead Survivor/Scripts/GameManger.cs
@@ -44,7 +44,9 @@ public class GameManger : MonoBehaviour
     }
     public void Gameover()
     {
+
         this.isLive = false;
+        
         Time.timeScale = 0;
         HUD.instance.GameResult(false);
         
